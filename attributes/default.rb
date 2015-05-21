11cfd2a5fc361ccc65139f63aa03a4843842db87
Eddie Hurtig <eddie@hurtigtechnologies.com>
2015-05-21 15:58:03 -0400

    Use the correct default elasticsearch cluster name

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default['elk_kibana']['cluster']['name'] = "elk-#{node.chef_environment}"
+default['elk_kibana']['cluster']['name'] = "#{node.chef_environment}-elk"
 set['kibana']['file']['config_template_cookbook'] = 'elk_kibana'
